c4d36162df42bf3e18c7ba266ef06724237a5b6b
Sesha <sesha@Admins-MacBook-Pro.local>
2017-07-14 15:27:42 -0700

    first python example

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello again"
+print "I like typing this."
+print "This is fun"
+print 'yay! Printing.'
+print "I'wd much rather you 'not'."
+print 'I "said" do not touch this.'
